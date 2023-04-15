ad97c6dba6988b4ced2a4c49a53b06d3c3c2a0b2
NilsRo <n.rottgardt@gmail.com>
2023-04-15 10:20:39 +0200

    - Added sleep function - Added ESP debugging (actually not working) - Has to check for Hardware issue with PowerLED

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 #include <DHT_U.h>
 #include <GRGB.h>
 #include "soc/rtc_wdt.h"
+#include <esp_core_dump.h>
+#include <jled.h>
 
 #define STRING_LEN 128
 #define nils_length(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
@@ -39,16 +41,17 @@ const int TOUCHLEDPIN = T5;
 const int TOUCHONOFFPIN = T4;
 
 bool running = false;
+bool sleeping = false;
 unsigned long sleepTime = 0;
 unsigned long sleepTimeMillis = 0;
-int thresholdOnOff = 27600;
+char sleepTimeStr[2];
+int thresholdOnOff = 27300;
 int thresholdLed = 31400;
 bool touchOnOff = false;
 bool touchLed = false;
 bool touchOnOffTrigger = false;
 bool touchLedTrigger = false;
 unsigned long touchMillis = 0;
-
 
 //DHT11 Sensor
 #define DHTTYPE    DHT11 
@@ -58,6 +61,10 @@ float humidity = 0.0;
 float humidity_max = 75.0;
 unsigned long timer10sMillis = 0;
 unsigned long timer1sMillis = 0;
+
+//Standard LED
+// auto led_breathe = JLed(LEDPPIN).Blink(1000, 500).Forever();
+
 
 //RGB LED
 GRGB led(COMMON_CATHODE, LEDRED, LEDGREEN, LEDBLUE);
@@ -112,6 +119,7 @@ IotWebConfTextParameter ntpServerParam = IotWebConfTextParameter("server", "ntpS
 IotWebConfTextParameter ntpTimezoneParam = IotWebConfTextParameter("timezone", "ntpTimezone", ntpTimezone, STRING_LEN, "CET-1CEST,M3.5.0/02,M10.5.0/03");
 IotWebConfParameterGroup dehumGroup = IotWebConfParameterGroup("dehum", "dehumidifier configuration");
 iotwebconf::FloatTParameter dehumHumidityThresholdParam = iotwebconf::Builder<iotwebconf::FloatTParameter>("dehumHumidityThresholdParam").label("humidity thresholdOnOff").defaultValue(55.0).step(0.5).placeholder("e.g. 55.5").build();
+IotWebConfNumberParameter sleepTimeParam = IotWebConfNumberParameter("sleep", "sleepTimeStr", sleepTimeStr, 2, "9");
 
 int mod(int x, int y)
 {
@@ -186,7 +194,9 @@ void handleRoot()
   s += "<td>";
   if (running)
     s += "running";
-  else 
+  else if (sleeping)
+    s += "sleeping";
+  else
     s += "stopped";
   s += "</td>";
   s += "</tr><tr>";
@@ -208,6 +218,12 @@ void handleRoot()
   s += "<br>";
   s += touchRead(TOUCHLEDPIN);
   s += iotWebConf.getHtmlFormatProvider()->getEnd();
+
+  if (esp_core_dump_image_check() == ESP_OK)
+    s += "Core Dump gefunden";
+  else
+    s += "kein Core Dump gefunden";
+
 
   server.send(200, "text/html", s);
 }
@@ -417,7 +433,7 @@ void updateLed()
   float humidity_thresholdOnOff = dehumHumidityThresholdParam.value();
   float percent = (humidity - humidity_thresholdOnOff) / (humidity_max - humidity_thresholdOnOff);
 
-  if (ledOnOff)
+  if (ledOnOff && !sleeping)
   {    
     if (percent > 1)
     {
@@ -468,16 +484,7 @@ void onTouchLed()
   touchLed = true;
 }
 
-void onSec1Timer()
-{
-  if (sleepTime >= millis() - sleepTimeMillis)
-    digitalWrite(LEDPPIN, !digitalRead(LEDPPIN));
-  else
-    digitalWrite(LEDPPIN, HIGH);
-  updateTime();
-}
-
-void onSec10Timer()
+void updateStatus()
 {
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -498,7 +505,7 @@ void onSec10Timer()
     humidity = event.relative_humidity;
   }
 
-  if (digitalRead(TRAYPIN) && sleepTime < millis() - sleepTimeMillis) 
+  if (digitalRead(TRAYPIN) && !(sleeping && (sleepTime < millis() - sleepTimeMillis))) 
   {
     if (!running && humidity > dehumHumidityThresholdParam.value()) {
       run(true);
@@ -509,16 +516,123 @@ void onSec10Timer()
   } else {
     run(false);
   }
+}
+
+void onSec1Timer()
+{
+  // if (sleepTime >= millis() - sleepTimeMillis)
+  //   digitalWrite(LEDPPIN, !digitalRead(LEDPPIN));
+  // else
+  //   digitalWrite(LEDPPIN, HIGH);
+  updateTime();
+}
+
+void onSec10Timer()
+{
+  updateStatus();
   updateLed();
   publishUptime();
   mqttSendtemp();
 }
+
+
+
+void readCoreDump()
+{
+  size_t size = 0;
+  size_t address = 0;
+  if (esp_core_dump_image_get(&address, &size) == ESP_OK)
+  {
+    const esp_partition_t *pt = NULL;
+    pt = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_COREDUMP, "coredump");
+
+    if (pt != NULL)
+    {
+      uint8_t bf[256];
+      char str_dst[640];
+      int16_t toRead;
+
+      for (int16_t i = 0; i < (size / 256) + 1; i++)
+      {
+        strcpy(str_dst, "");
+        toRead = (size - i * 256) > 256 ? 256 : (size - i * 256);
+
+        esp_err_t er = esp_partition_read(pt, i * 256, bf, toRead);
+        if (er != ESP_OK)
+        {
+          Serial.printf("FAIL [%x]\n",er);
+          //ESP_LOGE("ESP32", "FAIL [%x]", er);
+          break;
+        }
+
+        for (int16_t j = 0; j < 256; j++)
+        {
+          char str_tmp[3];
+
+          sprintf(str_tmp, "%02x", bf[j]);
+          strcat(str_dst, str_tmp);
+        }
+
+        printf("%s", str_dst);
+      }
+    }
+    else
+    {
+      Serial.println("Partition NULL");
+      //ESP_LOGE("ESP32", "Partition NULL");
+    }
+    // esp_core_dump_image_erase();
+  }
+  else
+  {
+    Serial.println("esp_core_dump_image_get() FAIL");
+    //ESP_LOGI("ESP32", "esp_core_dump_image_get() FAIL");
+  }
+}
+
+// esp_err_t esp_core_dump_image_erase()
+// {
+//     /* Find the partition that could potentially contain a (previous) core dump. */
+//     const esp_partition_t *core_part = esp_partition_find_first(ESP_PARTITION_TYPE_DATA,
+//                                                                 ESP_PARTITION_SUBTYPE_DATA_COREDUMP,
+//                                                                 "coredump");
+//     if (!core_part) {
+//         Serial.println("No core dump partition found!");
+//         return ESP_ERR_NOT_FOUND;
+//     }
+//     if (core_part->size < sizeof(uint32_t)) {
+//         Serial.println("Too small core dump partition!");
+//         return ESP_ERR_INVALID_SIZE;
+//     }
+
+//     esp_err_t err = ESP_OK;
+//     err = esp_partition_erase_range(core_part, 0, core_part->size);
+//     if (err != ESP_OK) {
+//         Serial.printf("Failed to erase core dump partition (%d)!\n", err);
+//         return err;
+//     }
+
+//     // on encrypted flash esp_partition_erase_range will leave encrypted
+//     // garbage instead of 0xFFFFFFFF so overwriting again to safely signalize
+//     // deleted coredumps
+//     const uint32_t invalid_size = 0xFFFFFFFF;
+//     err = esp_partition_write(core_part, 0, &invalid_size, sizeof(invalid_size));
+//     if (err != ESP_OK) {
+//         Serial.printf("Failed to write core dump partition size (%d)!\n", err);
+//     }
+
+//     return err;
+// }
+
+
 
 void setup()
 {
   rtc_wdt_set_length_of_reset_signal(RTC_WDT_SYS_RESET_SIG, RTC_WDT_LENGTH_3_2us);
   rtc_wdt_set_stage(RTC_WDT_STAGE0, RTC_WDT_STAGE_ACTION_RESET_SYSTEM);  
   rtc_wdt_set_time(RTC_WDT_STAGE0, 4000);
+  esp_core_dump_init();
+  
   // basic setup
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -583,6 +697,7 @@ void setup()
   iotWebConf.addParameterGroup(&ntpGroup);
 
   dehumGroup.addItem(&dehumHumidityThresholdParam);
+  dehumGroup.addItem(&sleepTimeParam);
   iotWebConf.addParameterGroup(&dehumGroup);
 
   iotWebConf.setConfigSavedCallback(&configSaved);
@@ -631,6 +746,8 @@ void setup()
   // configure the timezone
   configTime(0, 0, ntpServer);
   setTimezone(ntpTimezone);
+
+  sleepTime = atoi(sleepTimeStr);
   
   ArduinoOTA.begin();
   Serial.println("OTA Ready");
@@ -641,9 +758,11 @@ void setup()
 
 void loop()
 {
+  //Library handles
   iotWebConf.doLoop();
   ArduinoOTA.handle();
   rtc_wdt_feed();
+  // led_breathe.Update();
 
   if (10000 < millis() - timer10sMillis)
   {
@@ -653,7 +772,7 @@ void loop()
 
   if (1000 <  millis() - timer1sMillis)
   {
-    timer1sMillis = millis() + 1000;
+    timer1sMillis = millis();
     onSec1Timer();
   }
 
@@ -693,11 +812,11 @@ void loop()
 
   if (touchOnOff && touchOnOffTrigger)
   {
-    if (sleepTime > millis() - sleepTimeMillis)
-      sleepTime = 0;
-    else
-      sleepTime = 3600000;   // sleep 1 hour
+    sleeping = !sleeping;
+    sleepTimeMillis = millis();
     tone(BUZZERPIN, 1000, 500);
+    updateStatus();
+    updateLed();
     touchOnOffTrigger = false;
   }
   if (touchLed && touchLedTrigger)
